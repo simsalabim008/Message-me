5a9d985242e13dda50b9d743232309d07874eabf
simsalabim08 <westra77@gmail.com>
2019-11-04 15:08:12 +0100

    Show message body and username from database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Dikshy", password: "password")
+User.create(username: "Marijke", password: "password")
+User.create(username: "Tom", password: "password")
+User.create(username: "Mattie", password: "password")
